d339dc7ea76e7f978d472b1b6293c5528a4330cb
sanixto <sanix.to@gmail.com>
2023-02-17 23:39:17 +0200

    add basic document structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './Components/Header';
+import MainPage from './Components/MainPage';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <MainPage />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/MainPage.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const MainPage = () => {
+  return (
+    <main className='main'>
+      Main Page
+    </main>
+  )
+}
+
+export default MainPage;
