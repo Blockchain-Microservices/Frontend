f6dd925b207d8e2391dbe57966d8435c28c7cb29
sanixto <sanix.to@gmail.com>
2023-02-18 12:32:04 +0200

    add tokens db simulation

---
+++ src/Tokens.js
@@ -0,0 +1,64 @@
+const Tokens = [
+  {
+    id: 1,
+    name: 'TestCoin1',
+    symbol: 'BTC',
+    supplied: 10200.65,
+  },
+  {
+    id: 2,
+    name: 'TestCoin2',
+    symbol: 'TCN',
+    supplied: 30.235,
+  },
+  {
+    id: 3,
+    name: 'TestCoin3',
+    symbol: 'TCN',
+    supplied: 22.25,
+  },
+  {
+    id: 4,
+    name: 'TestCoin4',
+    symbol: 'BTC',
+    supplied: 10.233,
+  },
+  {
+    id: 5,
+    name: 'TestCoin5',
+    symbol: 'TCN',
+    supplied: 37.2,
+  },
+  {
+    id: 6,
+    name: 'TestCoin6',
+    symbol: 'BTC',
+    supplied: 123.25,
+  },
+  {
+    id: 7,
+    name: 'TestCoin7',
+    symbol: 'TCN',
+    supplied: 3.23,
+  },
+  {
+    id: 8,
+    name: 'TestCoin8',
+    symbol: 'TCN',
+    supplied: 27.75,
+  },
+  {
+    id: 9,
+    name: 'TestCoin9',
+    symbol: 'BTC',
+    supplied: 3.233,
+  },
+  {
+    id: 10,
+    name: 'TestCoin10',
+    symbol: 'TCN',
+    supplied: 95.7,
+  },
+];
+
+export default Tokens;
